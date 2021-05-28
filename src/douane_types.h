f67acd013be43179160765f8b82702969d0fcc34
john howard <orthopteroid@gmail.com>
2021-05-28 15:02:09 -0700

    remove file

--- src/douane_types.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DOUANE_TYPES_H_
-#define _DOUANE_TYPES_H_
-
-// eftirlit7 (gpl3) - orthopteroid@gmail.com
-// forked from douane-lkms (gpl3) - zedtux@zedroot.org
-
-
-
-#endif // _DOUANE_TYPES_H_
