8e693c622e2a522c03d9e130eb144b4e4b14867e
john howard <orthopteroid@gmail.com>
2021-05-30 13:26:19 -0700

    packetid alignment

--- src/module.h
+++ src/module.h
@@ -9,14 +9,14 @@
 #if 0
 
 #define LOG_INFO(id, fmt, ...) \
-  pr_info("I " MOD_NAME "(%u):%s:%d: " fmt "\n", id, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+  pr_info("I " MOD_NAME "(%08X):%s:%d: " fmt "\n", id, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 #define LOG_ERR(id, fmt, ...) \
-  pr_err("E " MOD_NAME "(%u):%s:%d: " fmt "\n", id, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+  pr_err("E " MOD_NAME "(%08X):%s:%d: " fmt "\n", id, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 #ifdef DEBUG
 #define LOG_DEBUG(id, fmt, ...) \
-  pr_debug("D " MOD_NAME "(%u):%s:%d: " fmt "\n", id, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+  pr_debug("D " MOD_NAME "(%08X):%s:%d: " fmt "\n", id, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
 #define LOG_DEBUG(fmt, ...) while(false)
 #endif
@@ -24,14 +24,14 @@
 #else
 
 #define LOG_INFO(id, fmt, ...) \
-  printk("I " MOD_NAME "(%u):%s:%d: " fmt "\n", id, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+  printk("I " MOD_NAME "(%08X):%s:%d: " fmt "\n", id, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 #define LOG_ERR(id, fmt, ...) \
-  printk("E " MOD_NAME "(%u):%s:%d: " fmt "\n", id, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+  printk("E " MOD_NAME "(%08X):%s:%d: " fmt "\n", id, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 #ifdef DEBUG
 #define LOG_DEBUG(id, fmt, ...) \
-  printk("D " MOD_NAME "(%u):%s:%d: " fmt "\n", id, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+  printk("D " MOD_NAME "(%08X):%s:%d: " fmt "\n", id, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
 #define LOG_DEBUG(fmt, ...) while(false)
 #endif
