b2bc0c7b13732bea88baba391cb908306b6deffc
John Howard <orthopteroid@gmail.com>
2021-06-01 15:52:30 -0700

    move rcu_lock inside sping_lock, 2x cache slots with the same number of keys

--- src/pi.c
+++ src/pi.c
@@ -39,13 +39,17 @@ enum nf_ip_hook_priorities {
 #define ALIGNED ____cacheline_aligned
 
 // cache size is paired with the key cutter size
-#define CACHE_SIZE 4096 // 12 bits
+#define CACHE_KEYS 4096 // 12 bits
 #define KEY_CUTTER(v) (v ^ (v >> 12) ^ (v >> 24))
+
+#define CACHE_FACTOR 2
+#define CACHE_SLOTS (CACHE_FACTOR * CACHE_KEYS)
+#define KEY_TO_SLOT(v) (CACHE_FACTOR * (KEY_CUTTER(v) % CACHE_KEYS))
 
 struct pi_cache
 {
-  unsigned long  ino[CACHE_SIZE] ALIGNED;
-  pid_t          pid[CACHE_SIZE] ALIGNED;
+  unsigned long  ino[CACHE_SLOTS] ALIGNED;
+  pid_t          pid[CACHE_SLOTS] ALIGNED;
 };
 
 struct pi_cache * pi_cache = 0;
@@ -63,35 +67,37 @@ bool pi_psi_from_ino(struct psi_struct * psi_out, unsigned long socket_ino, cons
   char * deleted_str = " (deleted)";
   int deleted_str_len = 10;
 
+  spin_lock_bh(&pi_lock);
   rcu_read_lock();
 
-  spin_lock_bh(&pi_lock);
-
-  j = KEY_CUTTER(socket_ino) % CACHE_SIZE;
-  for(i=0; i<CACHE_SIZE; i++, j++)
+  j = KEY_TO_SLOT(socket_ino);
+  for(i=0; i<CACHE_SLOTS; i++, j++)
   {
-    if(j == CACHE_SIZE) j = 0;
+    if(j == CACHE_SLOTS) j = 0;
     if(pi_cache->ino[j] == socket_ino)
     {
       struct pid * pid_struct = find_get_pid(pi_cache->pid[j]);
-      found_task = pid_struct ? get_pid_task(pid_struct, PIDTYPE_PID) : NULL;
-      found_pid = task->pid;
-
-      spin_unlock_bh(&pi_lock);
-
-      if(!found_task)
+      task = pid_struct ? get_pid_task(pid_struct, PIDTYPE_PID) : NULL;
+      if(!task)
       {
         LOG_ERR(packet_id, "invalid task info");
-        goto out_fail;
+        goto refresh_cache;
       }
+
+      found_task = task;
+      found_pid = pi_cache->pid[j];
 
       LOG_DEBUG(packet_id, "found INO %ld PID %d in cache", socket_ino, found_pid);
       goto out_found;
     }
   }
 
+  LOG_DEBUG(packet_id, "refreshing cache");
+
+refresh_cache:
+
   // clean cache
-  for(i=0; i<CACHE_SIZE; i++) pi_cache->ino[i] = 0;
+  for(i=0; i<CACHE_SLOTS; i++) pi_cache->ino[i] = 0;
 
   // refresh
   for_each_process(task)
@@ -104,50 +110,42 @@ bool pi_psi_from_ino(struct psi_struct * psi_out, unsigned long socket_ino, cons
       for(fd_i = 0; fd_i < fd_max; fd_i++)
       {
         struct file * file = fcheck_files(task->files, fd_i);
+        struct inode * inode = 0;
+
         if (!file) continue;
-        if (!S_ISSOCK(file_inode(file)->i_mode)) continue; // not a socket file
-        if (file_inode(file)->i_ino == socket_ino)
+        inode = file_inode(file);
+        if (!S_ISSOCK(inode->i_mode)) continue; // not a socket file
+        if (!found_task && !found_pid && (inode->i_ino == socket_ino))
         {
           found_task = task;
           found_pid = task->pid;
+
+          LOG_DEBUG(packet_id, "found INO %ld PID %d in process table", socket_ino, found_pid);
         }
 
-        j = KEY_CUTTER(file_inode(file)->i_ino) % CACHE_SIZE;
-        for(i=0; i<CACHE_SIZE; i++, j++)
+        j = KEY_TO_SLOT(inode->i_ino);
+        for(i=0; i<CACHE_SLOTS; i++, j++)
         {
-          if(j == CACHE_SIZE) j = 0;
+          if(j == CACHE_SLOTS) j = 0;
           if(pi_cache->ino[j] == 0)
           {
-            pi_cache->ino[j] = file_inode(file)->i_ino;
+            pi_cache->ino[j] = inode->i_ino;
             pi_cache->pid[j] = task->pid;
-            goto filled;
+            break;
           }
         }
-        LOG_ERR(packet_id, "unable to fill cache for INO %ld PID %d - cache too small?", file_inode(file)->i_ino, task->pid);
-
-filled:
-        ; // dear dog shoot me
+        if(i == CACHE_SLOTS)
+        {
+          LOG_ERR(packet_id, "unable to fill cache for INO %ld PID %d - cache too small?", file_inode(file)->i_ino, task->pid);
+        }
       }
     }
   }
 
-  spin_unlock_bh(&pi_lock);
-
-  if(found_task && found_task->mm && found_task->mm->exe_file)
-  {
-    LOG_DEBUG(packet_id, "found INO %ld PID %d in process table", socket_ino, found_pid);
-    goto out_found;
-  }
-
-out_fail:
-  LOG_DEBUG(packet_id, "searching for INO %ld - not found", socket_ino);
-
-  rcu_read_unlock();
-  return false;
-
 out_found:
-  {
-    char * p;
+  do {
+    char * p = 0;
+    if(!found_task || !(found_task->mm) || !(found_task->mm->exe_file)) break;
 
     // notes:
     // - d_path might return string with " (deleted)" suffix
@@ -156,7 +154,7 @@ out_found:
     if (IS_ERR(p))
     {
       LOG_ERR(packet_id, "d_path returned ERROR");
-      goto out_fail;
+      break;
     }
 
     psi_out->pid = found_pid;
@@ -165,29 +163,38 @@ out_found:
       // start of string is not start of buffer, so strip prefix
       strncpy(psi_out->process_path, p, PATH_LENGTH - (p - psi_out->process_path) +1); // +1 includes \0
     }
-  }
+
+    rcu_read_unlock();
+    spin_unlock_bh(&pi_lock);
+
+    // check for " (deleted)" suffix and strip it
+    name_str_len = strnlen(psi_out->process_path, PATH_LENGTH);
+    if (name_str_len > deleted_str_len)
+    {
+      // long enough for a suffix
+      int suffix_position = name_str_len - deleted_str_len;
+      if (0 == strncmp(psi_out->process_path + suffix_position, deleted_str, deleted_str_len))
+      {
+        memset(psi_out->process_path + suffix_position, 0, deleted_str_len);
+      }
+    }
+
+    return true;
+
+  } while(false);
+
+  LOG_DEBUG(packet_id, "searching for INO %ld - not found", socket_ino);
 
   rcu_read_unlock();
-
-  // check for " (deleted)" suffix and strip it
-  name_str_len = strnlen(psi_out->process_path, PATH_LENGTH);
-  if (name_str_len > deleted_str_len)
-  {
-    // long enough for a suffix
-    int suffix_position = name_str_len - deleted_str_len;
-    if (0 == strncmp(psi_out->process_path + suffix_position, deleted_str, deleted_str_len))
-    {
-      memset(psi_out->process_path + suffix_position, 0, deleted_str_len);
-    }
-  }
-  return true;
+  spin_unlock_bh(&pi_lock);
+  return false;
 }
 
 //////////////////
 
 int pi_init(void)
 {
-  LOG_INFO(0, "pi_cache %u entries %lu kb", CACHE_SIZE, sizeof(struct pi_cache) / 1024);
+  LOG_INFO(0, "pi_cache %u entries %lu kb", CACHE_SLOTS, sizeof(struct pi_cache) / 1024);
   pi_cache = kzalloc(sizeof(struct pi_cache), GFP_ATOMIC); // fixme
   if (!pi_cache)
   {
